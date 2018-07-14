1e33403e0f33be1d352ef39448aee34ffc2e7972
yoji0331 <smhsc251@yahoo.co.jp>
2018-07-14 17:20:19 +0900

    Add D29.js

---
+++ D/D29.js
@@ -0,0 +1,14 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var input_string = '';
+
+process.stdin.on('data', function(chunk) {
+  input_string = chunk;
+});
+
+process.stdin.on('end', function() {
+  var score = Number(input_string);
+
+  console.log(7-score);
+  }
+});
