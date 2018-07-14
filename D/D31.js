c64a333f712b44eda0600003c9d295c5080f92cf
yoji0331 <smhsc251@yahoo.co.jp>
2018-07-14 17:29:51 +0900

    Add D31.js

---
+++ D/D31.js
@@ -0,0 +1,13 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var input_string = '';
+
+process.stdin.on('data', function(chunk) {
+  input_string = chunk;
+});
+
+process.stdin.on('end', function() {
+  var value = Number(input_string);
+
+  console.log(value * 60);
+});
