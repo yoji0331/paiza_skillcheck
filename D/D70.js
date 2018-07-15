462af8925d6315671d313faa5e153fa8dbb92280
yoji0331 <smhsc251@yahoo.co.jp>
2018-07-15 20:13:45 +0900

    Add D70.js

---
+++ D/D70.js
@@ -0,0 +1,12 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var input_string = '';
+
+process.stdin.on('data', function(chunk) {
+  input_string = chunk;
+});
+
+process.stdin.on('end', function() {
+  var lines = input_string.split(' ');
+  console.log(lines[1] - lines[0]);
+});
