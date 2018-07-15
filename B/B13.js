870f39d36aa7fe07a45d114f81fa93436e3ea7be
yoji0331 <smhsc251@yahoo.co.jp>
2018-07-16 06:35:02 +0900

    Correct B13.js

--- B/B13.js
+++ B/B13.js
@@ -1,8 +1,9 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 var input_string = '';
-var eki;
+var jikoku;
 var a,b,c,N;
+var minutes;
 
 process.stdin.on('data', function(chunk) {
   input_string += chunk;
@@ -15,27 +16,29 @@ process.stdin.on('end', function() {
   b = Number(abc[1]);
   c = Number(abc[2]);
   N = Number(lines[1]);
-  eki = new Array(N);
+  jikoku = new Array(N);
   var ok = 0;
   for (var i=0;i<N;i++){
+    minutes = 0;
     var temp=lines[i+2];
     var hoge=temp.split(' ');
-    eki[i] = {hh: Number(hoge[0]),mm:Number(hoge[1])};
-    if(b+c+eki[i].hh*60+eki[i].mm < 540){
-      ok = i;
+    jikoku[i] = {hh: Number(hoge[0]),mm:Number(hoge[1])};
+    console.log(jikoku[i]);
+    var eki = jikoku[i].hh * 60 + jikoku[i].mm;
+    console.log(eki);
+    minutes = eki;
+    minutes += b;
+    minutes += c;
+    if(minutes < 540){
+      ok = eki - a;
+    } else {
+      break;
     }
 
   }
-  var ok_minutes=eki[ok].hh*60 + eki[ok].mm - a;
-  console.log(ok);
-  console.log(ok_minutes);
-  if(ok_minutes < 0){
-    ok_minutes = ok_minutes + 1440;
-  }
-
-  if(Math.floor(ok_minutes/60)> 10){
-    console.log(Math.floor(ok_minutes/60) + ":" + ok_minutes % 60);
+  if(minutes % 60 == 0){
+    console.log("0"+Math.floor(minutes/60) + ":0" + minutes % 60);
   } else{
-    console.log("0"+ Math.floor(ok_minutes/60) + ":" + ok_minutes % 60);
+    console.log("0"+Math.floor(minutes/60) + ":" + minutes % 60);
   }    
 });
